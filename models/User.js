bfc880664ebd9ca0d4851b4e34aab24bb87eae8f
Daniel Ty <tydaniel270@gmail.com>
2022-06-08 18:33:46 -0700

    changed user info contraints

--- models/User.js
+++ models/User.js
@@ -4,20 +4,16 @@ const UserSchema = new mongoose.Schema(
     username: {
       type: String,
       require: true,
-      min: 3,
-      max: 20,
       unique: true,
     },
     email: {
       type: String,
       require: true,
-      max: 20,
       unique: true,
     },
     password: {
       type: String,
       require: true,
-      min: 6,
     },
   },
   { timestamps: true }
