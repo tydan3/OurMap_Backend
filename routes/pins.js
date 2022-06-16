0602598e6e7894007d544c893335084821b21cdb
Daniel Ty <tydaniel270@gmail.com>
2022-06-16 00:04:16 -0700

    added delete pin method

--- routes/pins.js
+++ routes/pins.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const Pin = require("../models/Pin");
 
-//create a pin
-
+// create a pin
 router.post("/", async (req, res) => {
   const newPin = new Pin(req.body);
   try {
@@ -13,8 +12,28 @@ router.post("/", async (req, res) => {
   }
 });
 
-//get all pins
+// delete a pin
+router.delete("/", async (req, res) => {
+  // save pin to be deleted to pass as response
+  // for updating map
+  const deletedPin = await Pin.findOne({ _id: req.body._id });
+  if (!deletedPin) {
+    res.status(400).json("Cannot find pin!");
+  }
 
+  // remove pin
+  Pin.findByIdAndRemove(req.body._id)
+    .exec()
+    .then((doc) => {
+      if (!doc) {
+        return res.status(500).end();
+      }
+      return res.status(200).json(deletedPin);
+    })
+    .catch((err) => next(err));
+});
+
+// get all pins
 router.get("/", async (req, res) => {
   try {
     const pins = await Pin.find();
