cdbadd60bb55ededfb8a30af38c59f8df0bda024
Daniel Ty <tydaniel270@gmail.com>
2022-06-19 17:26:47 -0700

    made email optional

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.post("/register", async (req, res) => {
     //create new user
     const newUser = new User({
       username: req.body.username,
-      email: req.body.email,
+      email: body.email,
       password: hashedPassword,
     });
 
