df11e02b9f73acaf3fcde26a630734182b21b191
jtlu0930 <jtlu0930@gmail.com>
2022-09-30 09:59:24 -0400

    feat: added location to registration

--- models/User.js
+++ models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    location: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
